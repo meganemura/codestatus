2e8e757e01ea8a87e8b3cbf7610d48b9c1e0d03b
meganemura <meganemura@users.noreply.github.com>
2018-01-01 23:10:32 +0900

    Codestatus.status for specified repository

--- lib/codestatus.rb
+++ lib/codestatus.rb
@@ -6,14 +6,20 @@ require "codestatus/package_repository"
 require "codestatus/package_repository/github_repository"
 
 module Codestatus
-  def self.status(package_registry: :rubygems, package_name: 'apartment')
+  def self.status(slug = ARGV.first)
+    slug = 'rubygems/apartment' unless slug
+    package_registry, package_name = slug.split('/')
 
     resolver = RepositoryResolver.new(registry: package_registry, package: package_name)
 
     package_repository = resolver.repository
-
-    status = package_repository.status
-    success = (status == BuildStatus::SUCCESS)
+    if package_repository
+      status = package_repository.status
+      success = (status == BuildStatus::SUCCESS)
+    else
+      status = 'Repository not found'
+      success = 1
+    end
     puts status
     exit success ? 0 : 1
   end
