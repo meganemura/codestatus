50594cb02ce85cb58d86e7c727b2d9e3ea11ce49
meganemura <meganemura@users.noreply.github.com>
2018-01-06 13:44:35 +0900

    Implement `$ codestatus repository` to print repository html url

--- lib/codestatus/cli.rb
+++ lib/codestatus/cli.rb
@@ -23,5 +23,25 @@ module Codestatus
       end
       exit success ? 0 : 1
     end
+
+    desc "repository REGISTRY/PACKAGE", "Show repository of the package"
+    option :registry, type: :string, aliases: 'r'
+    option 'show-package-name', type: :boolean, default: false
+    def repository(slug)
+      if options[:registry]
+        package_registry = options[:registry]
+        package_name = slug
+      else
+        package_registry, package_name = slug.split('/', 2)
+      end
+
+      repository = Codestatus.repository(registry: package_registry, package: package_name)
+
+      if options['show-package-name']
+        puts "#{package_name}: #{repository.html_url}"
+      else
+        puts repository.html_url
+      end
+    end
   end
 end
